d79013e28032acb0fc98ca63a6f93f7740589f74
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-05-01 11:52:40 +0500

    main

--- app.js
+++ app.js
@@ -2,22 +2,36 @@ let reportBtn = document.getElementById("reportBtn")
 let reportMenu = document.getElementById("reportMenu")
 let closeBtn = document.getElementById("closeBtn")
 
+let reportBtnn = document.getElementById("reportBtnn")
+let reportMenuu = document.getElementById("reportMenuu")
 
-reportMenu.addEventListener("click", (e) => {
-    e.stopPropagation()
-})
+// hamma menu'larni yopish funksiyasi
+function closeAllMenus() {
+    reportMenu.classList.remove("show")
+    reportMenuu.classList.remove("show")
+}
 
+// first dropdown
 reportBtn.addEventListener("click", (e) => {
     e.stopPropagation()
+    closeAllMenus()
     reportMenu.classList.toggle("show")
 })
 
+// second dropdown
+reportBtnn.addEventListener("click", (e) => {
+    e.stopPropagation()
+    closeAllMenus()
+    reportMenuu.classList.toggle("show")
+})
+
+// close button
 closeBtn.addEventListener("click", (e) => {
     e.stopPropagation()
-    reportMenu.classList.remove("show")
+    closeAllMenus()
 })
 
+// outside click
 window.addEventListener("click", () => {
-    reportMenu.classList.remove("show")
+    closeAllMenus()
 })
-
